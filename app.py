c0f2fc138d25137350ef26ffd0b8bb2c8fc3ad28
srlog <srilog.ed@gmail.com>
2024-06-09 19:49:51 +0530

    Finished update 1

--- app.py
+++ app.py
@@ -14,11 +14,23 @@ socketio = SocketIO(app)
 rooms = {}
 users = {'Srinivas':'123','Lakshana':'234'}
 
+@app.route('/register',methods=["POST","GET"])
+def register():
+    if request.method== "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        if password == confirm_password :
+            users[username] = password
+            return redirect(url_for("login"))
+        else:
+            return render_template("register.html",error = "Password and confirm password doesn't match.")
+
+    return render_template("register.html")
+
 
 @app.route('/')
-def login1():
-    return render_template('login.html')
-
 @app.route('/login', methods=['POST','GET'])
 def login():
     if session.get("loggedin") :
@@ -27,7 +39,6 @@ def login():
         username = request.form.get("username")
         password = request.form.get('password')
         designation = request.form.get("designation", 'Student')
-        
         if username not in users.keys():
             error = "Username doesn't exist."
             return render_template("login.html",error=error)
@@ -64,7 +75,7 @@ def homepage():
 
 @app.route('/logout')
 def logout():
-    session.clear()
+    session['loggedin']=False
     return redirect(url_for('login'))
 
 
@@ -83,18 +94,17 @@ def generate_unique_code(length):
 def home():
     name = session.get("name")
     designation = session.get("designation")
-    session.clear()
     session["name"]= name
     session["designation"] = designation
     if request.method == "POST":
         name = session.get("name")
+        designation = session.get("designation")
+      
         topic = request.form.get("topic")
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
 
-        if not topic:
-            return render_template("select.html", error="Please enter a Topic.", code=code, topic=topic)
 
         if join != False and not code:
             return render_template("select.html", error="Please enter a room code.", code=code, topic=topic)
@@ -102,10 +112,12 @@ def home():
         room = code
         if create != False:
             room = generate_unique_code(6)
-            rooms[room] = {"topic":topic,"members": 0, "messages": []}
+            rooms[room] = {"designation":designation,"topic":topic,"members": 0, "messages": []}
         elif code not in rooms:
             return render_template("select.html", error="Room does not exist.", code=code, topic=topic)
         
+        if not topic:
+            return render_template("select.html", error="Please enter a Topic.", code=code, topic=topic)
         session["room"] = room
         session["name"] = name
         return redirect(url_for("room"))
@@ -115,19 +127,20 @@ def home():
 @app.route("/room")
 def room():
     room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
+    if room is None or session.get("name") is None or room not in rooms:    
         return redirect(url_for("home"))
 
-    return render_template("room.html", code=room, messages=rooms[room]["messages"], topic = rooms[room]["topic"])
+    return render_template("room.html", code=room, messages=rooms[room]["messages"], topic = rooms[room]["topic"], designation = session.get("designation"))
 
 @socketio.on("message")
 def message(data):
     room = session.get("room")
     if room not in rooms:
         return 
-    
+    print(rooms[room])
     content = {
         "name": session.get("name"),
+        "designation" : session.get("designation","Student"),
         "message": data["data"]
     }
     send(content, to=room)
@@ -138,14 +151,16 @@ def message(data):
 def connect(auth):
     room = session.get("room")
     name = session.get("name")
+    designation = session.get("designation")
     if not room or not name:
+
         return
     if room not in rooms:
         leave_room(room)
         return
     
     join_room(room)
-    send({"name": name, "message": "joined the conversation"}, to=room)
+    send({"name": name, "designation":designation,"message": "joined the conversation"}, to=room)
     rooms[room]["members"] += 1
     print(f"{name} joined the conversation {room}")
 
@@ -153,6 +168,7 @@ def connect(auth):
 def disconnect():
     room = session.get("room")
     name = session.get("name")
+    designation = session.get("designation")
     leave_room(room)
 
     if room in rooms:
@@ -160,7 +176,7 @@ def disconnect():
         if rooms[room]["members"] <= 0:
             del rooms[room]
     
-    send({"name": name, "message": "has left the conversation"}, to=room)
+    send({"name": name, "designation":designation, "message": "has left the conversation"}, to=room)
     print(f"{name} has left the conversation {room}")
 
 if __name__ == "__main__":
